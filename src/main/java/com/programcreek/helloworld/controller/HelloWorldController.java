a818a897b23529b0cf46bded805991747eb97464
jignesh01 <jignesh.sura01@gmail.com>
2016-11-10 13:14:02 -0500

    First commit

--- src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.programcreek.helloworld.controller;
  
+
+
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +13,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloWorldController {
 	String message = "Welcome to Spring MVC!";
  
+	private static Logger log = Logger.getLogger(HelloWorldController.class);
+	
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 		System.out.println("in controller");
- 
+
+		System.out.println("Test");
 		ModelAndView mv = new ModelAndView("helloworld");
 		mv.addObject("message", message);
 		mv.addObject("name", name);
